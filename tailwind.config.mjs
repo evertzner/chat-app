eb2fe58a68e195748344529226ae27bb5d9803ad
evertzner <esteban.vertzner@gmail.com>
2024-07-21 12:22:08 +0200

    style config. prettier

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      colors: {
+        'pale-violet': '#d89eff', // sub-heading at the top of the app UI
+        'moderate-violet': '#9241c8', // chat on the left
+        'desaturated-dark-violet': '#6e5d7e', // chat on the right
+        'grayish-blue': '#c6cacd', // placeholder text
+        'very-dark-desaturated-violet': '#3e2753', // main heading & submit button background
+        'dark-grayish-violet': '#a39daa', // paragraph
+        'light-magenta': '#e942ff',
+        'light-violet': '#8838ff',
+        white: '#ffffff',
+        'light-grayish-violet': '#f5f3f7', // app background
+        'very-light-magenta': '#e570ff' // radio button outline
+      }
+    }
+  },
+  plugins: []
+};
